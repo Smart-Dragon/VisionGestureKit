a2e28647a249bd84f7b9db81fb1bcfbfc8a4ce55
Masloboev Aleksei <supportalex@rambler.ru>
2021-11-29 21:09:38 +0300

    Initial Commit

---
+++ VisionGestureKit/VisionGestureKit.h
@@ -0,0 +1,18 @@
+//
+//  VisionGestureKit.h
+//  VisionGestureKit
+//
+//  Created by Алексей Маслобоев on 29.11.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VisionGestureKit.
+FOUNDATION_EXPORT double VisionGestureKitVersionNumber;
+
+//! Project version string for VisionGestureKit.
+FOUNDATION_EXPORT const unsigned char VisionGestureKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VisionGestureKit/PublicHeader.h>
+
+
